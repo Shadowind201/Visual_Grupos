1a775bea614c5316aa0c8a6d42a4d60d386226a3
Shadowind201 <enzocarnier@gmail.com>
2024-04-11 22:18:32 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Visual_Grupos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.anime1 = new System.Windows.Forms.RadioButton();
+            this.anime2 = new System.Windows.Forms.RadioButton();
+            this.anime3 = new System.Windows.Forms.RadioButton();
+            this.jogo1 = new System.Windows.Forms.RadioButton();
+            this.jogo2 = new System.Windows.Forms.RadioButton();
+            this.jogo3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.jogofoto3 = new System.Windows.Forms.PictureBox();
+            this.jogofoto2 = new System.Windows.Forms.PictureBox();
+            this.jogofoto1 = new System.Windows.Forms.PictureBox();
+            this.animefoto3 = new System.Windows.Forms.PictureBox();
+            this.animefoto2 = new System.Windows.Forms.PictureBox();
+            this.animefoto1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.jogofoto3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jogofoto2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jogofoto1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animefoto3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animefoto2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animefoto1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(103, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(87, 24);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Animes";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // anime1
+            // 
+            this.anime1.AutoSize = true;
+            this.anime1.Location = new System.Drawing.Point(278, 108);
+            this.anime1.Name = "anime1";
+            this.anime1.Size = new System.Drawing.Size(92, 24);
+            this.anime1.TabIndex = 7;
+            this.anime1.TabStop = true;
+            this.anime1.Text = "Anime 1";
+            this.anime1.UseVisualStyleBackColor = true;
+            this.anime1.Visible = false;
+            // 
+            // anime2
+            // 
+            this.anime2.AutoSize = true;
+            this.anime2.Location = new System.Drawing.Point(278, 254);
+            this.anime2.Name = "anime2";
+            this.anime2.Size = new System.Drawing.Size(92, 24);
+            this.anime2.TabIndex = 8;
+            this.anime2.TabStop = true;
+            this.anime2.Text = "Anime 2";
+            this.anime2.UseVisualStyleBackColor = true;
+            this.anime2.Visible = false;
+            // 
+            // anime3
+            // 
+            this.anime3.AutoSize = true;
+            this.anime3.Location = new System.Drawing.Point(278, 406);
+            this.anime3.Name = "anime3";
+            this.anime3.Size = new System.Drawing.Size(92, 24);
+            this.anime3.TabIndex = 9;
+            this.anime3.TabStop = true;
+            this.anime3.Text = "Anime 3";
+            this.anime3.UseVisualStyleBackColor = true;
+            this.anime3.Visible = false;
+            // 
+            // jogo1
+            // 
+            this.jogo1.AutoSize = true;
+            this.jogo1.Location = new System.Drawing.Point(742, 108);
+            this.jogo1.Name = "jogo1";
+            this.jogo1.Size = new System.Drawing.Size(82, 24);
+            this.jogo1.TabIndex = 10;
+            this.jogo1.TabStop = true;
+            this.jogo1.Text = "Jogo 1";
+            this.jogo1.UseVisualStyleBackColor = true;
+            this.jogo1.Visible = false;
+            // 
+            // jogo2
+            // 
+            this.jogo2.AutoSize = true;
+            this.jogo2.Location = new System.Drawing.Point(742, 254);
+            this.jogo2.Name = "jogo2";
+            this.jogo2.Size = new System.Drawing.Size(82, 24);
+            this.jogo2.TabIndex = 11;
+            this.jogo2.TabStop = true;
+            this.jogo2.Text = "Jogo 2";
+            this.jogo2.UseVisualStyleBackColor = true;
+            this.jogo2.Visible = false;
+            // 
+            // jogo3
+            // 
+            this.jogo3.AutoSize = true;
+            this.jogo3.Location = new System.Drawing.Point(742, 406);
+            this.jogo3.Name = "jogo3";
+            this.jogo3.Size = new System.Drawing.Size(82, 24);
+            this.jogo3.TabIndex = 12;
+            this.jogo3.TabStop = true;
+            this.jogo3.Text = "Jogo 3";
+            this.jogo3.UseVisualStyleBackColor = true;
+            this.jogo3.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 54);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(930, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 54);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(563, 30);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(77, 24);
+            this.radioButton8.TabIndex = 15;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Jogos";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // jogofoto3
+            // 
+            this.jogofoto3.Image = global::Visual_Grupos.Properties.Resources.dead_space;
+            this.jogofoto3.Location = new System.Drawing.Point(522, 356);
+            this.jogofoto3.Name = "jogofoto3";
+            this.jogofoto3.Size = new System.Drawing.Size(214, 124);
+            this.jogofoto3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.jogofoto3.TabIndex = 5;
+            this.jogofoto3.TabStop = false;
+            this.jogofoto3.Visible = false;
+            // 
+            // jogofoto2
+            // 
+            this.jogofoto2.Image = global::Visual_Grupos.Properties.Resources.helldivers;
+            this.jogofoto2.Location = new System.Drawing.Point(522, 208);
+            this.jogofoto2.Name = "jogofoto2";
+            this.jogofoto2.Size = new System.Drawing.Size(214, 124);
+            this.jogofoto2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.jogofoto2.TabIndex = 4;
+            this.jogofoto2.TabStop = false;
+            this.jogofoto2.Visible = false;
+            // 
+            // jogofoto1
+            // 
+            this.jogofoto1.Image = global::Visual_Grupos.Properties.Resources.death_stranding;
+            this.jogofoto1.Location = new System.Drawing.Point(522, 60);
+            this.jogofoto1.Name = "jogofoto1";
+            this.jogofoto1.Size = new System.Drawing.Size(214, 124);
+            this.jogofoto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.jogofoto1.TabIndex = 3;
+            this.jogofoto1.TabStop = false;
+            this.jogofoto1.Visible = false;
+            this.jogofoto1.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // animefoto3
+            // 
+            this.animefoto3.Image = global::Visual_Grupos.Properties.Resources.Mob_100;
+            this.animefoto3.Location = new System.Drawing.Point(58, 356);
+            this.animefoto3.Name = "animefoto3";
+            this.animefoto3.Size = new System.Drawing.Size(214, 124);
+            this.animefoto3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.animefoto3.TabIndex = 2;
+            this.animefoto3.TabStop = false;
+            this.animefoto3.Visible = false;
+            // 
+            // animefoto2
+            // 
+            this.animefoto2.Image = global::Visual_Grupos.Properties.Resources.JOJO;
+            this.animefoto2.Location = new System.Drawing.Point(58, 208);
+            this.animefoto2.Name = "animefoto2";
+            this.animefoto2.Size = new System.Drawing.Size(214, 124);
+            this.animefoto2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.animefoto2.TabIndex = 1;
+            this.animefoto2.TabStop = false;
+            this.animefoto2.Visible = false;
+            // 
+            // animefoto1
+            // 
+            this.animefoto1.BackColor = System.Drawing.SystemColors.Control;
+            this.animefoto1.Image = global::Visual_Grupos.Properties.Resources.full_metal;
+            this.animefoto1.Location = new System.Drawing.Point(58, 60);
+            this.animefoto1.Name = "animefoto1";
+            this.animefoto1.Size = new System.Drawing.Size(214, 124);
+            this.animefoto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.animefoto1.TabIndex = 0;
+            this.animefoto1.TabStop = false;
+            this.animefoto1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 559);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.jogo3);
+            this.Controls.Add(this.jogo2);
+            this.Controls.Add(this.jogo1);
+            this.Controls.Add(this.anime3);
+            this.Controls.Add(this.anime2);
+            this.Controls.Add(this.anime1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.jogofoto3);
+            this.Controls.Add(this.jogofoto2);
+            this.Controls.Add(this.jogofoto1);
+            this.Controls.Add(this.animefoto3);
+            this.Controls.Add(this.animefoto2);
+            this.Controls.Add(this.animefoto1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.jogofoto3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jogofoto2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jogofoto1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animefoto3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animefoto2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animefoto1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox animefoto1;
+        private System.Windows.Forms.PictureBox animefoto2;
+        private System.Windows.Forms.PictureBox animefoto3;
+        private System.Windows.Forms.PictureBox jogofoto1;
+        private System.Windows.Forms.PictureBox jogofoto2;
+        private System.Windows.Forms.PictureBox jogofoto3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton anime1;
+        private System.Windows.Forms.RadioButton anime2;
+        private System.Windows.Forms.RadioButton anime3;
+        private System.Windows.Forms.RadioButton jogo1;
+        private System.Windows.Forms.RadioButton jogo2;
+        private System.Windows.Forms.RadioButton jogo3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visual_Grupos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            animefoto1.Visible = true;
+            anime1.Visible = true;
+            animefoto2.Visible = true;
+            anime2.Visible = true;
+            animefoto3.Visible = true;
+            anime3.Visible = true;
+
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            jogofoto1.Visible = true;
+            jogo1.Visible = true;
+            jogofoto2.Visible = true;
+            jogo2.Visible = true;
+            jogofoto3.Visible = true;
+            jogo3.Visible = true;
+
+        }
+    }
+}
